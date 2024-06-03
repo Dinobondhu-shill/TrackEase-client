708d6fa17a79eae3daa2220b051acd1bf4675a26
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-06-03 12:51:42 +0600

    react router setup

---
+++ src/router/Router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-4xl">This is home</div>,
+  },
+]);
