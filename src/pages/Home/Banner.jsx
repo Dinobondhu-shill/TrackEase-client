eefd4fcbbf9f38ebad7544d7d9820bdd97c6461d
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-06-05 02:00:01 +0600

    about section added

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -19,6 +19,7 @@ const Banner = () => {
   >
     <SwiperSlide className="relative">
       <img src={slide1} className="w-full h-screen object-cover" />
+      <div className="w-full h-full bg-[#1c1b1b37] z-10 absolute top-0 left-0"></div>
       <div className="absolute top-2/4 ml-32 transform -translate-y-1/2  z-20">
         <h2 className="text-6xl mb-5 font-bold text-[#7be8f4da]">Join As A HR Manager!</h2>
         <p className="text-lg w-2/3  my-8 text-white">We're looking for a passionate and experienced HR Manager to help us grow and support our incredible team. If you're dedicated to fostering a positive work environment and have a knack for people management, we'd love to hear from you.</p>
@@ -27,8 +28,14 @@ const Banner = () => {
 
 
     </SwiperSlide>
-    <SwiperSlide>
+    <SwiperSlide className="relative">
+      <div className="w-full h-full bg-[#1c1b1b37] z-10 absolute top-0 left-0"></div>
       <img src={slide2} className="w-full h-screen object-cover" />
+      <div className="absolute top-2/4 ml-32 transform -translate-y-1/2  z-20">
+        <h2 className="text-6xl mb-5 font-bold text-[#7be8f4da]">Join As A Employee!</h2>
+        <p className="text-lg w-2/3  my-8 text-white">We are on the lookout for dedicated and talented individuals to join our team. Whether you are just starting your career or looking to advance, we offer a dynamic and supportive work environment. Be part of our exciting journey and make a difference with us.</p>
+        <Link className="px-4 py-2 border font-bold text-[#7be8f4da] border-blue-400 rounded-xl">Join As Employee</Link>
+      </div>
       </SwiperSlide>
   </Swiper>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
 import Banner from './Banner';
+import About from './About';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <About></About>
     </div>
   );
 };
