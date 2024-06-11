a3b1f1f90a12dccd8f328f83ad3f8a2189845a91
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-06-11 12:36:45 +0600

    employee added to hr team

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -1,14 +1,29 @@
+import axios from "axios";
 import useRoll from "../hooks/useRoll";
+import Swal from "sweetalert2";
 
-const EmployeeCard = ({person}) => {
+const EmployeeCard = ({person, refetch}) => {
   const [role] = useRoll()
   console.log(role)
   const company = role[2]
   const imageUrl2 = role[1]
   const packages = role[3]
-  
-  
-
+  const addingEmployee = {company, imageUrl2}
+  console.log(addingEmployee)
+   
+  const handleAddToTeam =async(id)=>{
+    const res = await axios.put(`http://localhost:5000/add-employee/${id}`, addingEmployee);
+    if(res.data.modifiedCount>0){
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "Employee has been added your Team",
+        showConfirmButton: false,
+        timer: 2000
+      });
+    }
+    refetch()
+  }
 return (
 <div className="border-2 rounded-xl bg-[#69a3a862] px-6 py-4 flex flex-col justify-center w-fit">
   <div className="avatar mb-4 flex items-center justify-center">
@@ -17,7 +32,7 @@ return (
     </div>
   </div>
   <h2 className="text-2xl font-bold">{person?.name}</h2>
-  <button className="border w-fit mx-auto py-2 px-3 rounded-md my-3 bg-[#69a3a840] font-semibold hover:bg-[#69a3a8a7] hover:text-white">Add to Team</button>
+  <button onClick={()=>handleAddToTeam(person._id)} className="border w-fit mx-auto py-2 px-3 rounded-md my-3 bg-[#69a3a840] font-semibold hover:bg-[#69a3a8a7] hover:text-white">Add to Team</button>
 </div>
 );
 };
